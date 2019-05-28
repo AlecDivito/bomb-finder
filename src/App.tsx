f9488a86205b7dabc878eea80307649831ad85ed
Alec Divito <divito.alec@gmail.com>
2019-05-27 22:21:34 -0400

    created project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from 'react';
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import Loading from './components/Loading';
+
+import Home from './pages/home';
+const About = lazy( () => import('./pages/about'));
+const GameMenu = lazy( () => import('./pages/game-menu'));
+const HowToPlay = lazy( () => import('./pages/how-to-play'));
+const Settings = lazy( () => import('./pages/settings'));
+const Game = lazy( () => import('./pages/game'));
+const GameWon = lazy( () => import('./pages/game-won'));
+const Stats = lazy( () => import('./pages/stats'));
+
+
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Suspense fallback={<Loading />}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/play" component={GameMenu} />
+          <Route path="/how-to-play" component={HowToPlay} />
+          <Route path="/settings" component={Settings} />
+          <Route path="/stats" component={Stats} />
+
+          {/* This one is special */}
+          <Route path="/game/:game" component={Game} />
+          <Route path="/game/:game/game-won" component={GameWon} />
+        </Switch>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
