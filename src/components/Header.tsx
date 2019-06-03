b208df89a0a3d2ac5b9384764cb470039fe1316b
Alec Divito <divito.alec@gmail.com>
2019-06-02 20:59:36 -0400

    updated header routing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import { Link, withRouter, RouteComponentProps } from 'react-router-dom';
-import "./Header.css"
 import back from "../assets/arrow-left.svg";
+import home from "../assets/home-f.svg";
+import "./Header.css"
 
 const Header: React.FC<RouteComponentProps> = (props: RouteComponentProps) => {
     // TODO: Check style if we need to render a hidden element or just null
     if (props.location.pathname === "/") {
         return <div className="header--hidden">BombFinder</div>;
     }
+    console.log(props);
+    const paths = props.location.pathname.split("/");
     return (
         <header className="header">
-            <Link to="/">
-                <img src={back} alt="Go To Main Menu" />
-            </Link>
+            <div className="header__link" 
+                onClick={() => props.history.goBack()}>
+                <img src={back} alt="Back" />
+            </div>
             <h3>
-                <Link to="/">
-                    BombFinder
-                </Link>
+                BombFinder
             </h3>
-            <Link to="/">
-                <img src={back} alt="delete me" />
+            <Link to="/" className="header__link">
+                <img src={home} alt="Home" />
             </Link>
         </header>
     );
